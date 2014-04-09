7a08cde678e31a1314716eb0d509118a8469791e
Brian Thomas Storti <btstorti@gmail.com>
2014-04-08 23:22:18 -0300

    functions to create the users table and insert a new user

---
+++ src/contas_gracie/db.clj
@@ -0,0 +1,29 @@
+; Keep in mind that I have no idea what I'm doing.
+
+(ns contas-gracie.db
+  (:require [clojure.java.jdbc :as sql]) ; use require to reference other clojure namespaces
+  (:import java.sql.DriverManager)) ; use import to reference Java classes
+
+(def db {:classname "org.sqlite.JDBC",
+         :subprotocol "sqlite",
+         :subname "db.sq3"})
+
+(defn create-users-table []
+  ; sql/with-connection ensures that the connection is properly cleaned up after use
+  (sql/with-connection
+    db
+    (sql/create-table
+      :users
+      [:id "INTEGER PRIMARY KEY AUTOINCREMENT"]
+      [:timestamp "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"]
+      [:email "TEXT"]
+      [:password "TEXT"])))
+
+; No, we're not going to save the password in plain text
+(defn insert-user [email password]
+  (sql/with-connection
+    db
+    (sql/insert-values
+      :users
+      [:email :password :timestamp]
+      [email password (new java.util.Date)])))
