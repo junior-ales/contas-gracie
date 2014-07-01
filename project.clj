e0424b89bf0d940ede99c1c0268413b67a1eda0f
Junior Ales <edilson.ales.jr@gmail.com>
2014-06-30 23:11:14 -0700

    Adding lein test-refresh to auto rerun tests outside repl

--- project.clj
+++ project.clj
@@ -10,8 +10,10 @@
                  [de.ubercode.clostache/clostache "1.3.1"]
                  [enlive "1.1.5"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]]
-  :plugins [[lein-bower "0.2.0"]]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.clojure/tools.namespace "0.2.4"]]
+  :plugins [[lein-bower "0.2.0"]
+            [com.jakemccrary/lein-test-refresh "0.5.0"]]
   :bower-dependencies [[foundation "5.2.1"]]
   :bower {:directory "resources/public/bower_components"}
   :main contas-gracie.core)
