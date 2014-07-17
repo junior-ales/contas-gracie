74f587d4c3134bd0280587e135e599bb4a6715e9
Junior Ales <edilson.ales.jr@gmail.com>
2014-07-16 20:14:18 -0700

    Moving DB access functions to theirs respectives namespaces

---
+++ src/contas_gracie/models/db.clj
@@ -0,0 +1,17 @@
+(ns contas-gracie.models.db
+  (:require [clojure.java.jdbc :as sql])
+  (:import java.sql.DriverManager)) ; use import to reference Java classes
+
+(def db {:classname "org.sqlite.JDBC",
+         :subprotocol "sqlite",
+         :subname "db.sq3"})
+
+(defn create-users-table []
+  (sql/with-connection db
+    (sql/create-table
+      :users
+      [:id "INTEGER PRIMARY KEY AUTOINCREMENT"]
+      [:timestamp "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"]
+      [:email "TEXT"]
+      [:password "TEXT"])))
+
--- src/contas_gracie/models/user.clj
+++ src/contas_gracie/models/user.clj
@@ -1,4 +1,6 @@
-(ns contas-gracie.models.user)
+(ns contas-gracie.models.user
+  (:require [clojure.java.jdbc :as sql]
+            [contas-gracie.models.db :refer [db]]))
 
 ;; TODO fetch users from database
 (def users
@@ -6,3 +8,16 @@
           {:name "desiree" :bills-responsible-for ["rent" "electricity"] :payer? true}
           {:name "brian" :bills-responsible-for ["internet"] :payer? false})))
 
+; No, we're not going to save the password in plain text
+(defn insert-user [email password]
+  (sql/with-connection db
+    (sql/insert-values
+      :users
+      [:email :password :timestamp]
+      [email password (new java.util.Date)])))
+
+(defn user-exists? [email password]
+  (<= 1 (sql/with-connection db
+    (sql/with-query-results*
+      [(str "SELECT * FROM users WHERE email = '" email "' AND password = '" password "'")]
+      (fn [x] (count x))))))
